ecf4a369431f5caab9453f34a8a17e1c6768c961
ikramovmuhammadziyo <m994853818@gmail.com>
2024-05-03 16:01:15 +0500

    asdffsda

---
+++ test.py
@@ -0,0 +1,17 @@
+import time
+
+print("10%")
+time.sleep(1)
+print("30%")
+time.sleep(1)
+print("50%")
+time.sleep(1)
+print("75%")
+time.sleep(1)
+print("80%")
+time.sleep(1)
+print("95%")
+time.sleep(1)
+print("100%")
+time.sleep(1)
+print("You hacked NASA")
